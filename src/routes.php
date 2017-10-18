e2c3a6b20230534750f26fb5ea002234fce3f52b
Jan Novak <nowitz.jan@gmail.com>
2017-10-18 22:31:12 +0200

    oprava updatu flipper

--- src/routes.php
+++ src/routes.php
@@ -196,7 +196,7 @@ $app->patch('/flipper/{name}/{id}', function (Request $request, Response $respon
         $result = 2;
     } else {
 //      je to OK, update zaznamu
-        if($body["test"] == 1){
+        if($request->getParsedBody()["test"] == 1){
             $result = $this->dibi->query('UPDATE flipper SET ', ['open' => 1], 'WHERE id_name = %s AND id = %s', $args['name'], $args['id']);
         }
     }
