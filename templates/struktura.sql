7923c17d412173c0b827a1156c2c6e8841efef13
Jan Novak <nowitz.jan@gmail.com>
2017-10-28 20:23:28 +0200

    db skripty

---
+++ templates/struktura.sql
@@ -0,0 +1,59 @@
+create table calendar
+(
+	id int auto_increment	primary key,
+	name varchar(128) not null comment 'Nazev kalendare, musi byt jedinecnej.',
+	password varchar(128) not null comment 'Heslo ke kalendari.',
+	background longtext not null comment 'Obrazek v base64 kodovani',
+	template tinyint default '0' not null comment '0-nejedna se o sablonu / 1-jedna se o sablonu',
+	constraint id	unique (id),
+	constraint name	unique (name)
+);
+
+create index id_calendar	on calendar (id);
+create index name_calendar on calendar (name);
+
+create table flipper
+(
+	id int auto_increment	primary key,
+	id_name varchar(128) null,
+	front longtext null,
+	text text null,
+	back longtext null,
+	date timestamp default CURRENT_TIMESTAMP not null,
+	open tinyint(1) default '0' null,
+	conditional tinyint(1) default '0' null,
+	text_color varchar(64) default 'white' not null,
+	border_color varchar(64) default 'white' not null,
+	constraint id	unique (id),
+	constraint flipper_calendar_name_fk
+		foreign key (id_name) references calendar (name)
+);
+
+create index id_flipper	on flipper (id);
+create index id_name_flipper on flipper (id_name);
+
+create table user
+(
+	id int auto_increment	primary key,
+	id_name varchar(128) not null comment 'odkaz na kalendar',
+	email varchar(128) not null comment 'email',
+	cost tinyint(1) not null comment '0-free / 1 - pracenej',
+	cost_size int(10) not null comment 'vyse castky',
+	constraint id	unique (id)
+);
+
+create index id_user	on user (id);
+create index id_name_user	on user (id_name);
+
+create table warning
+(
+	id int auto_increment	primary key,
+	id_name varchar(128) not null,
+	text text not null,
+	constraint id	unique (id),
+	constraint warning_calendar_fk
+		foreign key (id_name) references calendar (name)
+);
+
+create index id_warning	on warning (id);
+create index id_name_warning	on warning (id_name);
